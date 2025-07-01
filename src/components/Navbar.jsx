eadb10f6c1501cfea7d9c17f8deb241f95ca7d2e
Florian Cherubin <floriancherubin@gmail.com>
2025-07-01 19:18:43 +0200

    ajout du link accueil/log et A PROPOS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="w-full flex justify-between items-center pt-[var(--padding-m)]">
-      <span className="logo">leaflife</span>
+      <Link to="/">
+        <span className="logo">leaflife</span>
+      </Link>
       <div className="flex gap-[40px]">
-        <Link to="/about">ABOUT</Link>
+        <Link to="/about">A PROPOS</Link>
         <Link to="/services">SERVICES</Link>
         <Link to="/projects">PROJECTS</Link>
         <Link to="/contact">CONTACT</Link>
