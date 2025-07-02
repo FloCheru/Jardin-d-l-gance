28b88088e1f4b07ae838a0442c01cfd27604295c
Florian Cherubin <floriancherubin@gmail.com>
2025-07-02 19:32:56 +0200

    màj home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,54 +16,77 @@ export default function Home() {
       <div className="background-overlay" />
 
       {/* Contenu principal centré */}
-      <div className="relative z-20 flex flex-col justify-between gap-[var(--container-padding)] w-full h-full ">
+      <div className="relative z-20 flex flex-col justify-between gap-[var(--padding-m)] w-full h-full ">
         <Navbar />
         {/* Bloc Titre, description et 500+ Satisfied Clients */}
         <div className="flex justify-between items-start">
           {/* Titre et description */}
           <div className="flex flex-col justify-center items-start w-[65%]">
             <h1 className="hero-title mb-6">
-              <span className="hero-title-white">CREATE YOUR</span>
+              <span className="hero-title-white">CRÉEZ VOTRE</span>
               <br />
-              <span className="hero-title-lightgray ">DREAM GARDEN</span>
+              <span className="hero-title-lightgray ">JARDIN DE RÊVE</span>
             </h1>
             <p className="hero-desc w-full">
-              Create dream gardens with passion, creativity and sustainability
-              for over a decade with our experienced landscape artists and
-              gardener teams.
+              Créons ensemble des jardins d'exception avec passion, créativité
+              et durabilité depuis plus de 15 ans grâce à notre équipe
+              expérimentée d'architectes paysagistes et de jardiniers.
             </p>
           </div>
-
-          {/* Bloc "500+ Satisfied Clients" */}
-          <div className="flex flex-col items-start gap-2 satisfied-clients p-[var(--button-padding)]">
-            <span className="font-semibold text-[var(--color-secondary)]">
-              500+
-            </span>
-            <span className="text-[var(--color-secondary)] font-[var(--font-weight-regular)] ">
-              Satisfied Clients
-            </span>
-          </div>
-        </div>
-        {/* Bloc boutons + Hachioji Garden */}
-        <div className="flex justify-between items-start">
-          <div className="flex gap-[20px]">
-            <Button variant="secondary">Get Started</Button>
-            <Button variant="tertiary">Explore Projects</Button>
-          </div>
-          <div className="z-21 relative flex flex-col items-center gap-[20px] p-[var(--container-padding)] w-[40%] bg-white">
-            {/* Overlay sombre pour lisibilité */}
-            <div className="background-overlay background-overlay-hachioji" />
-            <div className=" flex w-full justify-between items-center gap-2">
-              <MapPin color="var(--color-secondary)" />
-              <Button variant="arrow" />
-            </div>
-            <div>
-              <div className="font-semibold text-[var(--color-secondary)]">
-                Hachioji Garden
+          {/* Contenu principal centré */}
+          <div className="relative z-20 flex flex-col justify-between gap-[var(--padding-m)] w-full h-full ">
+            <Navbar />
+            {/* Bloc Titre, description et 500+ Satisfied Clients */}
+            <div className="flex justify-between items-start">
+              {/* Titre et description */}
+              <div className="flex flex-col justify-center items-start w-[65%]">
+                <h1 className="hero-title mb-6">
+                  <span className="hero-title-white">CRÉEZ VOTRE</span>
+                  <br />
+                  <span className="hero-title-lightgray ">JARDIN DE RÊVE</span>
+                </h1>
+                <p className="hero-desc w-full">
+                  Créons ensemble des jardins d'exception avec passion,
+                  créativité et durabilité depuis plus de 15 ans grâce à notre
+                  équipe expérimentée d'architectes paysagistes et de
+                  jardiniers.
+                </p>
               </div>
-              <div className="font-normal text-[var(--color-secondary)]">
-                We design Hachioji Garden as a part of our new Landscape Design
-                Commission in the country
+
+              {/* Bloc "500+ Satisfied Clients" */}
+              <div className="flex flex-col items-start gap-s satisfied-clients p-[var(--padding-s)]">
+                <span className="font-semibold text-[var(--color-secondary)]">
+                  500+
+                </span>
+                <span className="text-[var(--color-secondary)] font-[var(--font-weight-regular)] ">
+                  Clients Satisfaits
+                </span>
+                <div className="flex relative">
+                  <div className="satisfied-clients-circle"></div>
+                  <div className="satisfied-clients-circle !-translate-x-4"></div>
+                  <div className="satisfied-clients-circle !-translate-x-8"></div>
+                </div>
+              </div>
+            </div>
+            {/* Bloc boutons + Hachioji Garden */}
+            <div className="flex justify-between items-start">
+              <div className="flex gap-[20px]">
+                <Button variant="secondary">Commencer</Button>
+                <Button variant="tertiary">Découvrir nos Projets</Button>
+              </div>
+              <div>
+                <div className="font-semibold text-[var(--color-secondary)]">
+                  Jardin Méditerranéen
+                </div>
+                <div>
+                  <div className="font-semibold text-[var(--color-secondary)]">
+                    Jardin Méditerranéen
+                  </div>
+                  <div className="font-normal text-[var(--color-secondary)]">
+                    Réalisation complète d'un aménagement paysager contemporain
+                    alliant tradition provençale et modernité
+                  </div>
+                </div>
               </div>
             </div>
           </div>
